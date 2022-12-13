679a93c8cfd7717773b05e04c896d35d378db7b6
ibeneme <ibenemeikenna@96gmail.com>
2022-12-13 18:43:01 +0100

    update on port

--- index.js
+++ index.js
@@ -131,10 +131,6 @@ app.get('/logout', function(req, res){
 
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-
 
 app.listen(port, function(){
   console.log("Server has started successfully.");
